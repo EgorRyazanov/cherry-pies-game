2e19dba6c5de018512a041fd5232e3777158f3a4
Egor Ryazanov <egor.ryazanov@saritasa.com>
2023-12-30 19:39:23 +0500

    Mock match media to fix jest error

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -12,6 +12,20 @@ global.fetch = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve('hey') })
 )
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
+
 test('Example test', async () => {
   render(
     <Provider store={store}>

eede7a7e504064ff5c186bb8d2dedc806c348e1c
Sherstneva Tatyana <nikolina.tatyana@yandex.ru>
2023-12-31 12:02:45 +0500

    изменила настройки db.ts на сервере на прежние

--- packages/server/src/db.ts
+++ packages/server/src/db.ts
@@ -2,8 +2,8 @@ import { Sequelize } from 'sequelize'
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
-  username: 'postgres',
-  password: 'postgres',
+  username: 'root',
+  password: 'root',
   host: 'localhost',
   port: 5432,
   database: 'my_db',
